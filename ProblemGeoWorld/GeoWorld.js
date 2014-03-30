1ddceb905a85754ada904cbf6cf1cb66e1b9ab84
Vivian Leung <vivianleung19@gmail.com>
2014-03-30 05:00:05 +0100

    end

--- ProblemGeoWorld/GeoWorld.js
+++ ProblemGeoWorld/GeoWorld.js
@@ -1,7 +1,8 @@
 /**
  * Created by hen on 3/8/14.
  */
-var margin, width, height, bbVis, svg, projectionMethods, actualProjectionMethod, colorMin, colorMax, dataSet;
+var margin, width, height, bbVis, svg, projectionMethods, actualProjectionMethod, currInd;
+var colorScale, lScale, lAxis, legendDom, lTick, legend, world;
 
 margin = { top: 50, right: 50, bottom: 50, left: 50 };
 width = 960 - margin.left - margin.right;
@@ -12,9 +13,8 @@ projectionMethods = [ //.precision(.1);
     { name:"mercator", method: d3.geo.mercator().translate([width / 2, height / 2]) },
     { name:"equiRect", method: d3.geo.equirectangular().translate([width / 2, height / 2]) },
     { name:"stereo",   method: d3.geo.stereographic().translate([width / 2, height / 2]) }];
-actualProjectionMethod = projectionMethods.length-1;
+actualProjectionMethod = 0;
 colorScale = d3.scale.quantize().range(colorbrewer.Greens[5]);
-dataSet = {};
 
 svg = d3.select("#vis").append("svg")
     .attr({width:width+margin.left+margin.right,height:height+margin.top+margin.bottom})
@@ -24,31 +24,112 @@ svg = d3.select("#vis").append("svg")
 
 
 var getCountryIDs = function(file, callback) { 
-    d3.json(file, function(world) {
+    d3.json(file, function(w) {
         $.ajax({
         url: "http://api.worldbank.org/countries?format=jsonP&prefix=Getdata&per_page=500",
         jsonpCallback:'getdata',
         dataType:'jsonp',
         success: function( data, status) {
             var codes = {};
-            data[1].forEach(function(d) { codes[d.id] = d.iso2Code})
-            world.features.forEach(function(d) {
-                if (codes[d.id]) {d.id = codes[d.id];}
+            data[1].forEach(function(d) { codes[d.id] = d})
+            w.features.forEach(function(d) {
+                if (codes[d.id]) {
+                    d.id = codes[d.id].iso2Code;
+                    d.properties.country = codes[d.id];
+                }
             });
-            callback(null, world);
+            callback(null, w);
         }})
     })
 } 
 
-var initVis = function(error, indicators, world){
+var initVis = function(error, indicators, map){
+    world = map;
     svg.append('g').attr('id','countries').selectAll(".country")
         .data(world.features).enter().append('path')
-        .attr('class','country');
-    // var IndicatorNames = indicators.map(function(d){ return d.IndicatorName})
-    // $( "#indicator" ).autocomplete({source: IndicatorNames});
+        .attr('class','country')
+        .on("click", updateLabel);
+    // indicator selection input and menu
+    var timer, term, hidden;
+    var indList = d3.select('select[name="filtered"]')
+    .selectAll('.ind').data(indicators).enter()
+    .append("option").attr("value", function(d,i){return d.IndicatorCode})
+    .text(function(d) {return d.IndicatorName})
+    .on("click", function(){ 
+        currInd = {id: this.value, value: this.text};
+        runAQueryOn(currInd.id) });
 
 
-    runAQueryOn('SH.TBS.INCD');
+    d3.selectAll(".settings input").on("keyup",function() {
+        if (this.id=="selectorYear") {runAQueryOn(currInd.id);}
+        else if (this.id=="indicator" && this.value != term) {
+            term = this.value;
+            if (timer) {clearTimeout(timer);}
+            timer = setTimeout(function() {
+                if (hidden) {hidden.forEach(function(d){$(d).unwrap( );})}
+                indList.style("display","initial");
+                if (term) {
+                    hidden = indList.filter( function(d) {
+                        return d.IndicatorName.toLowerCase().indexOf(term.toLowerCase()) == -1;})[0]
+                    hidden.forEach(function(d) {$(d).wrap("<span style='display: none;'></span>");})
+                }
+            }, 500);
+        }
+    })
+    // sets up legend
+    legendDom = function() {
+        var r = colorScale.range().map(function(d) {return colorScale.invertExtent(d)[0]});
+        r.push(colorScale.domain()[1]);
+        return r;
+    }
+    lTick = {w: 50, p:5};
+    lScale = d3.scale.linear().domain([0,1]).range([0, 100]);
+    lAxis = d3.svg.axis().orient("right").tickSize(lTick.w+lTick.p);
+
+    // proxy indicator
+    currInd = {id: null, value: "Country Index # (Alphabetical)"};
+    world.features.forEach(function(d,i) {
+        d.properties.indValue = {value: i}
+    })
+    colorScale.domain([0, d3.keys(world.features).length-1])
+    changePro();
+
+}
+
+var runAQueryOn = function(indicatorString) {
+    var indDate = d3.select('input[id="selectorYear"]')[0][0].value;
+    if (!parseInt(indDate)) {indDate = ""}
+    else { indDate = "&date="+indDate}
+    $.ajax({
+        url: "http://api.worldbank.org/countries/all/indicators/"+indicatorString+"?format=jsonP&prefix=Getdata"+indDate+"&MRV=1&per_page=500", //do something here
+        jsonpCallback:'getdata',
+        dataType:'jsonp',   
+        success: function (data, status){
+            var inDom = [Number.MAX_VALUE, 0];
+            console.log(data)
+            currInd = data[1][0].indicator;
+            world.features.forEach(function(d) {
+                delete d.properties.indValue;
+                data[1].forEach(function(e) {
+                    if (d.id == e.country.id) {
+                        if (e.value || e.decimal) { 
+                            if (e.value == null) {e.value = 0;}
+                            e.value = parseFloat(e.value + "." + e.decimal); 
+                            if (e.value < inDom[0]) {inDom[0] = e.value;}
+                            else if (e.value > inDom[1]) {inDom[1] = e.value;}
+                        }
+                        d.properties.indValue = {value: e.value, date: parseInt(e.date)};
+                    }
+                })
+            })
+            colorScale.domain(inDom);
+            changePro();
+        },
+        error: function() {
+            alert("Invalid Year.")
+        }
+
+    });
 }
 
 queue()
@@ -56,79 +137,67 @@ queue()
     .defer(getCountryIDs, "../data/world_data.json")
     .await(initVis);
 
-            // times = d3.keys(dataSet[0].properties.hourly);
-            // yDom = d3.extent(d3.values(dataSet[0].properties.hourly));
-            // xScale = d3.scale.ordinal().domain(times).rangeRoundBands([0, bbDetail.w],.15,0);
-            // yScale = d3.scale.linear().domain(yDom).range([bbDetail.h, 0]);
-            // colorHour = d3.scale.quantile().domain(d3.range(24))
-            //   .range(['#E66545','#FF9933','#FFD633','#FFD633','#FF9933','#E66545']);
-
-
-
-// just for fun 
 var textLabel = svg.append("text").text(projectionMethods[actualProjectionMethod].name).attr({
     "transform":"translate(-40,-30)"
 })
 
 var changePro = function(){
-    actualProjectionMethod = (actualProjectionMethod+1) % (projectionMethods.length);
 
     textLabel.text(projectionMethods[actualProjectionMethod].name);
     path= d3.geo.path().projection(projectionMethods[actualProjectionMethod].method);
     svg.selectAll(".country").transition().duration(750)
     .attr("d",function(d) {return path(d.geometry)})
     .attr("fill",function(d) {
-        if (dataSet[d.id]) { return colorScale(dataSet[d.id].value); }
+        if (d.properties.indValue ) { return colorScale(d.properties.indValue.value); }
         else {return 'grey'}
     });
-    var lRange = colorScale.range().map(function(d) {return colorScale.invertExtent(d)[0]})
-    lRange.push(colorScale.domain()[1]);
-    var lTick = {w: 50, p:5};
-    var lScale = d3.scale.linear().domain(colorScale.domain()).range([0, 100]);
-    var lAxis = d3.svg.axis().scale(lScale).orient("right").tickSize(lTick.w+lTick.p).tickValues(lRange);
-    var legend = svg.append("g").attr({class:"key", transform:"translate(-40,"+
-            (height-margin.top-margin.bottom-2*lTick.p+30)+")"});
+    // .on("click", zoomed);
+    
+    lScale.domain(colorScale.domain());
+    lDom = legendDom();
+    lAxis.scale(lScale).tickValues(lDom);
+
+    if (legend) {legend.remove();}
+
+    legend = svg.append("g").attr({class:"key", transform:"translate(-40,"+
+        (height-margin.top-margin.bottom-2*lTick.p+30)+")"});
     legend.selectAll("rect")
         .data(colorScale.range().map(function(d,i) {
-          return { y0: lScale(lRange[i]), y1: lScale(lRange[i+1]), z: d };
+          return { y0: lScale(lDom[i]), y1: lScale(lDom[i+1]), z: d };
         }))
         .enter().append("rect")
         .attr("width", lTick.w)
         .attr("y", function(d) { return d.y0; })
         .attr("height", function(d) { return d.y1 - d.y0; })
         .style("fill", function(d) { return d.z; });
+    legend.call(lAxis).append('text')
+        .attr({class:"caption","alignment-baseline":"before-edge", y:lScale.range()[1]+2*lTick.p})
+        .text(currInd.value);
 
-    legend.call(lAxis).append("text")
-        .attr("class", "caption").attr({"alignment-baseline":"before-edge", y:lScale.range()[1]+2*lTick.p})
-        .text(dataSet.indicator.value);
-        
 
 };
 
-d3.select("body").append("button").text("changePro").on({"click":changePro});
 
-function runAQueryOn(indicatorString) {
-    $.ajax({
-        url: "http://api.worldbank.org/countries/all/indicators/"+indicatorString+"?format=jsonP&prefix=Getdata&per_page=500&date=2000", //do something here
-        jsonpCallback:'getdata',
-        dataType:'jsonp',
-        success: function (data, status){
-            var inDom = [Number.MAX_VALUE, 0];
-            dataSet.indicator = data[1][0].indicator;
-            data[1].forEach(function(d){ 
-                if (d.value) { 
-                    d.value = parseFloat(d.value + "." + d.decimal); 
-                    if (d.value < inDom[0]) {inDom[0] = d.value;}
-                    else if (d.value > inDom[1]) {inDom[1] = d.value;}
-                }
+d3.select("body").append("button").text("changePro").on("click",function() {    
+    actualProjectionMethod = (actualProjectionMethod+1) % (projectionMethods.length);
+    changePro();});
 
-                d.date = parseInt(d.date);
-                delete d.decimal;
-                dataSet[d.country.id] = d;  
-            });
-            colorScale.domain(inDom);
-                changePro();
 
-        }
-    });
+function updateLabel() {
+    console.log(d3.select(this).data()[0])
+    var info = d3.select(this).data()[0];
+        if (info.properties.indValue) {
+        var date;
+        if(info.properties.indValue.date){date = info.properties.indValue.date;}
+        else {date = "N/A";}
+        var value;
+        if(info.properties.indValue.value){value = info.properties.indValue.value;}
+        else {value = "N/A";}
+        d3.select('#textLabel').html(
+            info.properties.name.toUpperCase()+" (ISO: "+info.id+")<br/>"+
+            "Year: "+date+"<br/>"+
+            currInd.value+": "+value
+        )   
+    }
+
 }
